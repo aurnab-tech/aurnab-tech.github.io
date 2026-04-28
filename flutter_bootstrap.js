17330ffd3c24a928c9356b6ca94167b40bc1cbf9
Aurnab Das <aurnab.das@technonext.com>
2026-04-28 16:21:19 +0600

    version 1.0.4
    - with splash screen and with both local and remote config json

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1266220926"
+    serviceWorkerVersion: "3657473872"
   }
 });
